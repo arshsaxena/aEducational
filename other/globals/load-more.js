0a3010c33579d13f622ca0a8f0db962a9817f4b3
Arsh Saxena <arsh.saxena02@gmail.com>
2022-02-21 19:13:47 +0530

    updated

--- other/globals/load-more.js
+++ other/globals/load-more.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
-    $(".blog-links-container").slice(0, 10).show();
+    $(".blog-links").slice(0, 10).show();
     $("#loadMore").on("click", function(e){
         e.preventDefault();
-        $(".blog-links-container:hidden").slice(0, 10).slideDown();
-        if($(".blog-links-container:hidden").length == 0) {
+        $(".blog-links:hidden").slice(0, 10).slideDown();
+        if($(".blog-links:hidden").length == 0) {
             $("#loadMore").text("That's all!").addClass("noContent");
         }
     });
